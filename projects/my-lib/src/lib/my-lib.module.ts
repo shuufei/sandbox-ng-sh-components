bc29c57ef55d479f007db8e08e3e7426d2f68444
shuufei <digital.shuufei@gmail.com>
2019-08-05 21:45:41 +0900

    Initial commit.

---
+++ projects/my-lib/src/lib/my-lib.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MyLibComponent } from './my-lib.component';
+import { ButtonComponent } from './components/button/button.component';
+
+
+
+@NgModule({
+  declarations: [MyLibComponent, ButtonComponent],
+  imports: [
+  ],
+  exports: [MyLibComponent]
+})
+export class MyLibModule { }
