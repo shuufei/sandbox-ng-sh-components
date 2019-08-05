0cdb22ecae920bf9d4f3a5221726f406d2da3468
shuufei <digital.shuufei@gmail.com>
2019-08-05 23:42:51 +0900

    Add storybook.

---
+++ stories/index.stories.ts
@@ -0,0 +1,31 @@
+import { storiesOf, moduleMetadata } from '@storybook/angular';
+import { Button } from '@storybook/angular/demo';
+
+import { MyLibModule } from 'dist/my-lib';
+
+const metadata = {
+  declarations: [],
+  imports: [
+    MyLibModule
+  ]
+};
+
+storiesOf('My Button', module)
+  .add('with text', () => ({
+    component: Button,
+    props: {
+      text: 'Hello Button',
+    },
+  }))
+  .add('with emoji', () => ({
+    component: Button,
+    props: {
+      text: '😀 😎 👍 💯',
+    },
+  }));
+
+storiesOf('MyLibButton', module)
+  .addDecorator(moduleMetadata(metadata))
+  .add('default', () => ({
+    template: `<lib-button></lib-button>`
+  }));
